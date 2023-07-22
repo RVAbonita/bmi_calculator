8e1702225b5513449b00517b8015491be747b16c
Reishsier Abonita <113200403+RVAbonita@users.noreply.github.com>
2023-07-23 00:14:39 +0800

    Update internet_speedtest.py

--- internet_speedtest.py
+++ internet_speedtest.py
@@ -1,29 +1,38 @@
-import tkinter as tk
+import streamlit as st
 
-class App(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack()
+# Function to calculate BMI
+def calculate_bmi(weight, height):
+    height_meters = height / 100
+    bmi = weight / (height_meters ** 2)
+    return bmi
 
-        self.entrythingy = tk.Entry()
-        self.entrythingy.pack()
+st.title('BMI Calculator')
 
-        # Create the application variable.
-        self.contents = tk.StringVar()
-        # Set it to some value.
-        self.contents.set("this is a variable")
-        # Tell the entry widget to watch this variable.
-        self.entrythingy["textvariable"] = self.contents
+# Input for weight in kilograms
+weight = st.number_input('Enter your weight (kg)', min_value=1.0, max_value=300.0, step=0.1)
 
-        # Define a callback for when the user hits return.
-        # It prints the current value of the variable.
-        self.entrythingy.bind('<Key-Return>',
-                             self.print_contents)
+# Input for height in centimeters
+height = st.number_input('Enter your height (cm)', min_value=1.0, max_value=300.0, step=0.1)
 
-    def print_contents(self, event):
-        print("Hi. The current entry content is:",
-              self.contents.get())
+# Calculate BMI
+if st.button('Calculate BMI'):
+    bmi_result = calculate_bmi(weight, height)
+    st.write(f'Your BMI is: {bmi_result:.2f}')
 
-root = tk.Tk()
-myapp = App(root)
-myapp.mainloop()
+    # Interpret BMI result
+    if bmi_result < 18.5:
+        st.warning('You are underweight.')
+    elif 18.5 <= bmi_result < 24.9:
+        st.success('You have a normal weight.')
+    elif 25.0 <= bmi_result < 29.9:
+        st.warning('You are overweight.')
+    else:
+        st.error('You are obese.')
+
+# Let's add a BMI chart for reference
+st.header('BMI Chart')
+st.image('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/bmi-chart.jpg', use_column_width=True)
+
+# Show the footer
+st.write('---')
+st.write('Created by Your Name')

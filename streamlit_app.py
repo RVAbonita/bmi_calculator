3c06041512f456dd5c2a9168df229e84f59e07ce
Reishsier Abonita <113200403+RVAbonita@users.noreply.github.com>
2023-07-23 18:43:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,31 @@ def calculate_bmi(weight, height):
 
 st.title('BMI Calculator')
 
+# Input for weight in kilograms
+weight = st.number_input('Enter your weight (kg)', min_value=1.0, max_value=300.0, step=0.1)
+
+# Input for height in centimeters
+height = st.number_input('Enter your height (cm)', min_value=1.0, max_value=300.0, step=0.1)
+
+# Calculate BMI
+if st.button('Calculate BMI'):
+    bmi_result = calculate_bmi(weight, height)
+    st.write(f'Your BMI is: {bmi_result:.2f}')
+
+    # Interpret BMI result
+    if bmi_result < 18.5:
+        st.warning('You are underweight.')
+    elif 18.5 <= bmi_result < 24.9:
+        st.success('You have a normal weight.')
+    elif 25.0 <= bmi_result < 29.9:
+        st.warning('You are overweight.')
+    else:
+        st.error('You are obese.')
+
+# Let's add a BMI chart for reference
+st.header('BMI Chart')
+st.image('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/bmi-chart.jpg', use_column_width=True)
+
+# Show the footer
+st.write('---')
+st.write('Created by Your Name')

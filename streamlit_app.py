715ee5aabccadd1ac168ba4a419fb0719e95a625
Reishsier Abonita <113200403+RVAbonita@users.noreply.github.com>
2023-07-23 18:55:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 # Function to convert height from feet and inches to centimeters
 def convert_to_cm(height_str):
-    feet, inches = map(int, height_str.split("'"))
-    height_cm = feet * 30.48 + inches * 2.54
-    return height_cm
+    try:
+        feet, inches = map(int, height_str.split("'"))
+        height_cm = feet * 30.48 + inches * 2.54
+        return height_cm
+    except ValueError:
+        return None
 
 # Function to calculate BMI
 def calculate_bmi(weight, height):
@@ -18,14 +21,10 @@ st.title('BMI Calculator')
 weight = st.number_input('Enter your weight (kg)', min_value=1.0, max_value=300.0, step=0.1)
 
 # Input for height in feet and inches
-height_str = st.text_input('Enter your height (e.g., 5\'5)')
+height_str = st.text_input('Enter your height (e.g., 5\'5")')
 
-def convert_to_cm(height_str):
-    try:
-        feet, inches = map(int, height_str.split("'"))
-        height_cm = feet * 30.48 + inches * 2.54
-        return height_cm
-    except ValueError:
+# Convert height to centimeters
+height_cm = convert_to_cm(height_str)
 
 # Calculate BMI
 if st.button('Calculate BMI'):
@@ -43,17 +42,4 @@ if st.button('Calculate BMI'):
         else:
             st.error('You are obese.')
     else:
-        st.warning("Please enter your height in the format 'feet'inches\", for example, 5'5\".")
-
-# BMI Chart as a table
-st.header('BMI Chart')
-bmi_chart_data = {
-    'BMI Category': ['Underweight', 'Normal weight', 'Overweight', 'Obese'],
-    'BMI Range': ['< 18.5', '18.5 - 24.9', '25.0 - 29.9', '>= 30.0'],
-    'Interpretation': ['You are underweight.', 'You have a normal weight.', 'You are overweight.', 'You are obese.']
-}
-st.table(bmi_chart_data)
-
-# Show the footer
-st.write('---')
-st.write('Created by Shir')
+        st.warning("Please enter your height in the format
